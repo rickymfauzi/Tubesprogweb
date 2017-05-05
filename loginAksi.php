890fb62b2e4a2b2be35e28a3eef8be72bc30775e
rickymfauzi <rickymuhammadfauzi@gmail.com>
2017-05-05 20:15:57 +0700

    Penambahan kalimat agar lebih baik
    Sudah fix

--- loginAksi.php
+++ loginAksi.php
@@ -8,7 +8,7 @@ $QLogin = mysql_query("SELECT * FROM admin WHERE u = '$u' AND p = '$p'");
 $JLogin	= mysql_num_rows($QLogin);
 
 if ($JLogin != 1) {
-	echo "<div id='gagal'>Login Gagal. Periksa kembali</div>";
+	echo "<div id='gagal'>Maaf Login Gagal. Periksa kembali</div>";
 } else {
 	$AUser = mysql_fetch_array($QLogin);
 	$_SESSION['username'] 	= $AUser[1];
